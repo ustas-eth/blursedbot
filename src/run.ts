b117ad0fe8bbb9ff34d7053c85fa6a339350c115
OSvol <somethingtrash@riseup.net>
2023-03-07 11:46:20 +0000

    Delay

--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,8 @@
-import { blur } from './config.js'
+import { blur, timings } from './config.js'
 import { init } from './libs/tallyho.js'
 import { login } from './Blur/login.js'
+import { delay } from './libs/utils.js'
+import { randomInt } from 'crypto'
 import { BasicStrategyCollection } from './Blur/strategies/basic.js'
 
 await init()
@@ -9,4 +11,5 @@ await login()
 for (const { name, address } of blur.collections) {
   const collection = new BasicStrategyCollection(name, address)
   collection.openCollection()
+  await delay(randomInt(timings.start.min, timings.start.max))
 }
