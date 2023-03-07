e9575baac30d6a48098aed767088a9bbce32f17b
OSvol <somethingtrash@riseup.net>
2023-03-07 11:51:21 +0000

    JsonRpcProvider to FallbackProvider, error capture

--- src/libs/ethers.ts
+++ src/libs/ethers.ts
@@ -1,9 +1,13 @@
-import { BigNumberish, Contract, ethers, JsonRpcProvider, Wallet } from 'ethers'
-import { blur, providerUrl, seed } from '../config.js'
+import { BigNumberish, Contract, ethers, FallbackProvider, JsonRpcProvider, Wallet } from 'ethers'
+import { blur, providerUrls, SEED } from '../config.js'
+import { logger } from './logs.js'
 
-const provider = new JsonRpcProvider(providerUrl)
+const providers = []
+for (const url of providerUrls) providers.push(new JsonRpcProvider(url))
 
-export const wallet = Wallet.fromPhrase(seed, provider)
+const provider = new FallbackProvider(providers)
+
+export const wallet = Wallet.fromPhrase(SEED, provider)
 
 const contract = new Contract(
   blur.contract,
@@ -12,6 +16,13 @@ const contract = new Contract(
 )
 
 export async function getBalance(): Promise<string> {
-  const inWei: BigNumberish = await contract.getFunction('balanceOf')(wallet.address)
-  return ethers.formatEther(inWei)
+  try {
+    const inWei: BigNumberish = await contract.getFunction('balanceOf')(wallet.address)
+    return ethers.formatEther(inWei)
+  } catch (error) {
+    logger.error(error)
+    return '0'
+  }
 }
+
+console.log(await getBalance())
